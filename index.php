f299087f7793ea133f990cce8214974f21065792
mfelipedias <mrs.flp@gmail.com>
2022-05-07 18:02:26 -0300

    pagina inicial

---
+++ index.php
@@ -0,0 +1,220 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- fontawesome Icons -->
+    <script src="https://kit.fontawesome.com/17040bbfa3.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Sistema | Acorde</title>
+
+    <style>
+        .hover {
+            color: #1673f7
+        }
+
+        .hover:hover {
+            color: #E74C3C
+        }
+    </style>
+    <link href="http://fonts.cdnfonts.com/css/my-big-heart-demo" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/magistral-honesty" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/the-chatalestick" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/klubkatz" rel="stylesheet">
+
+
+
+</head>
+
+<body class="bg-light" style="max-width: 1366px; margin-right:auto; margin-left:auto">
+    <div class="row">
+        <header class="bg-white navbar shadow rounded d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-1 mb-2 border-bottom" style=" margin-right:auto; margin-left:auto">
+            <a href="#" class="d-flex align-items-center col-md-3 mb-md-0 text-dark text-decoration-none">
+                <img src="./img/coruja.png" width="40" role="img" class="img-fluid">
+                <div class="row" style="height:30px;margin:0px">
+                    <p style="margin-left:8px;margin-bottom: 0; padding: 0;line-height: 1; color:#1673f7;font-family: 'Magistral Honesty', sans-serif;">Sistema</p>
+                    <p style="margin-top: 0; padding: 0;line-height: 1.1;font-size: 30px;background: linear-gradient(to right, #f32170, #ff6b08,#cf23cf, #eedd44); -webkit-text-fill-color: transparent;-webkit-background-clip: text;font-family: 'Magistral Honesty', sans-serif;">ACORDE</p>
+                </div>
+            </a>
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Início</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Alunos</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Matérias</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Tutores</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
+            </ul>
+            <div class="col-md-3 text-end">
+                <div class="dropdown text-end">
+                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <label style="margin-right:15px;">Bem vindo, <b style="color:#E74C3C;">Felipe!</b></label>
+                        <img src="https://github.com/mdo.png" alt="mdo" width="35" height="35" class="rounded-circle">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end text-small" aria-labelledby="dropdownUser1">
+                        <li><a class="dropdown-item" href="#">Meu perfil</a></li>
+                        <li><a class="dropdown-item" href="#">Mudar senha</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Sair</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    </div>
+    <main style="max-width: 1366px;  margin-right:auto; margin-left:auto">
+        <div class="row mb-3">
+            <div class="card" style="background-image: url(./img/pen.png);background-size: 130px; background-repeat: no-repeat; background-position: 0% 50%; ">
+                <div class="card-body" style="padding-bottom: 0; background-image: url(./img/logo_escola.png);background-size: 120px; background-repeat: no-repeat; background-position: 99% 50% ">
+                    <center>
+                        <p class="lead text-muted" style="font-size:25px;font-family: 'My Big Heart Demo', sans-serif; ">E.E. Profª. Hermelina de Albuquerque Passarela</p>
+                        <p>
+                            <a class="bi hover bi-vector-pen mx-2" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" data-bs-content="Criar Aviso" href="#" style="font-size: 30px;"></a>
+                            <a class="bi  hover bi-circle-half mx-2" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" data-bs-content="Alto Contraste" href="#" style="font-size: 30px;"></a>
+                            <a class="bi hover bi-facebook mx-2" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" data-bs-content="Facebook" href="#" style="font-size: 30px;"></a>
+                            <a class="bi hover bi-instagram mx-2" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" data-bs-content="Instagram" href="#" style="font-size: 30px;"></a>
+                            <a class="bi hover bi-envelope-paper mx-2" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="bottom" data-bs-content="Email" href="#" style="font-size: 30px;"></a>
+                        </p>
+                    </center>
+                </div>
+                <center>
+                    <div class="row" style="height: 5px; background: rgb(48,65,135); background: linear-gradient(90deg, rgba(48,65,135,1) 0%, rgba(22,115,247,1) 21%);;">&nbsp</div>
+                    <div class="row" style="height: 7px; background: #E74C3C;">&nbsp</div>
+                </center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="">
+                    <h5><i class="bi bi-chat-left-dots" style="margin-right: 10px;"></i>Avisos</h5>
+                    <hr>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                            <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                <div>
+                                    <h6 class="mb-0">Título</h6>
+                                    <p class="mb-0 opacity-75">Conteúdo da mensagem.</p>
+                                </div>
+                                <small class="opacity-50 text-nowrap">01/01/2022</small>
+                            </div>
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                            <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                <div>
+                                    <h6 class="mb-0">Título</h6>
+                                    <p class="mb-0 opacity-75">Conteúdo da mensagem.</p>
+                                </div>
+                                <small class="opacity-50 text-nowrap">01/01/2022</small>
+                            </div>
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                            <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                <div>
+                                    <h6 class="mb-0">Título</h6>
+                                    <p class="mb-0 opacity-75">Conteúdo da mensagem.</p>
+                                </div>
+                                <small class="opacity-50 text-nowrap">01/01/2022</small>
+                            </div>
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                            <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                <div>
+                                    <h6 class="mb-0">Título</h6>
+                                    <p class="mb-0 opacity-75">Conteúdo da mensagem.</p>
+                                </div>
+                                <small class="opacity-50 text-nowrap">01/01/2022</small>
+                            </div>
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                            <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+                            <div class="d-flex gap-2 w-100 justify-content-between">
+                                <div>
+                                    <h6 class="mb-0">Título</h6>
+                                    <p class="mb-0 opacity-75">Conteúdo da mensagem.</p>
+                                </div>
+                                <small class="opacity-50 text-nowrap">01/01/2022</small>
+                            </div>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-xl-6">
+                <h5><i class="bi bi-info-square" style="margin-right: 10px;"></i>Informaçoes gerais</h5>
+                <hr>
+                <div class="card" style="height: 381px; background-image: linear-gradient(rgba(255,255,255,0.9), rgba(255,255,255,0.9)), url(./img/coruja.png);background-repeat: no-repeat;">
+                    <div class="card-body">
+                        <div class="row" style="margin-left: 0px;">
+                            <p style="margin-left:8px;margin-bottom: 0; padding: 0;line-height: 1; color:#1673f7;font-family: 'Magistral Honesty', sans-serif;">Sistema</p>
+                            <p style="margin-top: 0; margin-bottom: 0; padding: 0;line-height: 1.1;font-size: 30px;background: linear-gradient(to right, #f32170, #ff6b08,#cf23cf, #eedd44); -webkit-text-fill-color: transparent;-webkit-background-clip: text;font-family: 'Magistral Honesty', sans-serif;">ACORDE</p>
+                        </div>
+                        <p style="line-height: 1">(Acompanhamento de Resultados e Demandas Especiais)</p>
+                        
+                        <p>Projeto Integrador II</p>
+                        <p>Sistema para acompanhamento de desempenho de alunos com TDAH</p>
+                      
+                            <small>
+                                ALEC ALEXANDRE GRACELLI, RA 2007086<br>
+                                GABRIELA CALLIGARIS KAYATT, RA 2004226<br>
+                                DIEGO DA SILVA COSTA, RA 2015273<br>
+                                LEANDRO DONIZETE DA SILVA, RA 2002583<br>
+                                JOÃO FÁBIO SIQUEIRA DINIZ, RA 2006613<br>
+                                MARCOS FELIPE ALMEIDA DIAS DA SILVA, RA 2004989<br>
+                                RODRIGO OCTAVIO LOPES MENDES, RA 2005427
+                            </small>
+                   
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 text-muted">&copy; 2021 Company, Inc</p>
+
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img src="./img/logo_univesp.png" width="150px" role="img" class="img-fluid" alt="...">
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Início</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Alunos</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Matérias</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Tutores</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Sobre</a></li>
+        </ul>
+    </footer>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+    <script>
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+    </script>
+</body>
+
+</html>
