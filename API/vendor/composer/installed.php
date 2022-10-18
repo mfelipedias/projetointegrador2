e387b6fb070bf602a6eeb34608220bde6e2e24b2
mfelipedias <43459240+mfelipedias@users.noreply.github.com>
2022-10-18 19:10:10 -0300

    composer update

--- API/vendor/composer/installed.php
+++ API/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c31063a2317484c091ce2835f2f69380c47230a2',
+        'reference' => 'f9c8d17518e396ebd019987107d3750a6fbb7806',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c31063a2317484c091ce2835f2f69380c47230a2',
+            'reference' => 'f9c8d17518e396ebd019987107d3750a6fbb7806',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
