5540a0bb099adc346b146b43e9d2c6f26a209598
mfelipedias <43459240+mfelipedias@users.noreply.github.com>
2022-10-18 20:33:07 -0300

    JSON CORRIGIDO
    json no padrao correto

--- API/App/Models/Aluno.php
+++ API/App/Models/Aluno.php
@@ -10,15 +10,15 @@ class Aluno
     public static function select(int $id)
     {
 
-        $connPdo = new \PDO('mysql:host=acorde.mysql.uhserver.com;dbname=acorde', 'acorde', 'Acorde.102030');
+        $connPdo = new \PDO('mysql:host=acorde.mysql.uhserver.com;dbname=acorde', 'acorde', 'Acorde.102030', array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        
         $sql = 'SELECT * FROM ' . self::$table . ' WHERE id_aluno = :id';
         $stmt = $connPdo->prepare($sql);
         $stmt->bindValue(':id', $id);
         $stmt->execute();
 
         if ($stmt->rowCount() > 0) {
-            $result = $stmt->fetch(\PDO::FETCH_ASSOC);
-            print_r($result);
+            return $stmt->fetch(\PDO::FETCH_ASSOC);
         } else {
             throw new \Exception("Nenhum aluno encontrato!");
         }
@@ -26,15 +26,14 @@ class Aluno
 
     public static function selectAll()
     {
-        $connPdo = new \PDO('mysql:host=acorde.mysql.uhserver.com;dbname=acorde', 'acorde', 'Acorde.102030');
+        $connPdo = new \PDO('mysql:host=acorde.mysql.uhserver.com;dbname=acorde', 'acorde', 'Acorde.102030', array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
         $sql = 'SELECT * FROM ' . self::$table;
         $stmt = $connPdo->prepare($sql);
         $stmt->execute();
 
         if ($stmt->rowCount() > 0) {
-            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            print_r($result);
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
         } else {
             throw new \Exception("Nenhum aluno encontrado!");
         }
--- API/public_html/index.php
+++ API/public_html/index.php
@@ -1,28 +1,31 @@
 <?php
-header('Content-Type: application/json');
-require_once '../vendor/autoload.php';
+    header('Content-Type: application/json');
 
-if ($_GET['url']) {
-    $url = explode('/', $_GET['url']);
+    require_once '../vendor/autoload.php';
 
-    if ($url[0] === 'api') {
-        array_shift($url);
+    // api/users/1
+    if ($_GET['url']) {
+        $url = explode('/', $_GET['url']);
 
-        $service = 'App\Services\\'.ucfirst($url[0]).'Service';
-        array_shift($url);
+        if ($url[0] === 'api') {
+            array_shift($url);
 
-        $method = strtolower($_SERVER['REQUEST_METHOD']);
+            $service = 'App\Services\\'.ucfirst($url[0]).'Service';
+            array_shift($url);
 
-        try {
-            $response = call_user_func_array(array(new $service, $method), $url);
+            $method = strtolower($_SERVER['REQUEST_METHOD']);
 
-            http_response_code(200);
-            echo json_encode(array('status' => 'sucess', 'data' => $response));
-            exit;
-        } catch (\Exception $e) {
-            http_response_code(404);
-            echo json_encode(array('status' => 'error', 'data' => $e->getMessage()), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
-            exit;
+            try {
+                $response = call_user_func_array(array(new $service, $method), $url);
+
+                http_response_code(200);
+                echo json_encode(array('status' => 'sucess', 'data' => $response), JSON_UNESCAPED_UNICODE);
+                exit;
+            } catch (\Exception $e) {
+                http_response_code(404);
+                echo json_encode(array('status' => 'error', 'data' => $e->getMessage()), JSON_UNESCAPED_UNICODE);
+                exit;
+            }
         }
     }
-}
+    
